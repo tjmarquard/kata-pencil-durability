f96a9ba55b86c50241c37136e85d9dcbe797d3d8
Tim Marquard <tjmarquard@gmail.com>
2019-11-29 12:54:54 -0600

    degrade point for lower case words

--- src/Pencil.py
+++ src/Pencil.py
@@ -4,6 +4,12 @@ class Pencil:
         self.writtenText = ""
         self.pointDurability = pointDurability
 
-    def write(self, wordString):
-        self.writtenText += wordString
+    def write(self, text):
+        self.writtenText += text
         return self.writtenText
+
+    def degradePoint(self, text):
+        for char in text:
+            if char.islower():
+                self.pointDurability -= 1
+        return self.pointDurability
--- tests/test_Pencil.py
+++ tests/test_Pencil.py
@@ -2,7 +2,7 @@ import unittest
 
 from src.Pencil import Pencil
 
-# python -m unittest tests.test_Pencil.TestPencil
+# python -m unittest tests.test_Pencil.TestPencilWrite
 class TestPencilWrite(unittest.TestCase):
     
     def test_write_oneword(self):
@@ -19,3 +19,11 @@ class TestPencilWrite(unittest.TestCase):
     def test_set_default_point_durability(self):
         pencil = Pencil()
         self.assertEqual(pencil.pointDurability, 20)
+
+# python -m unittest tests.test_Pencil.TestPencilDegradePoint
+class TestPencilDegradePoint(unittest.TestCase):
+
+    def test_points_left_to_write_after_writing_a_lowercase_word(self):
+        pencil = Pencil()
+        pencil.degradePoint("booger")
+        self.assertEqual(pencil.pointDurability, 14)
